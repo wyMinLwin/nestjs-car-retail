3fa50f146fab64c5243e21db95f75f6fea5af1b2
wyMinLwin <waiyanminlwin421@gmail.com>
2024-01-02 23:10:13 +0630

    Custom Data Serialization

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { Observable, map } from 'rxjs';
+
+interface ClassConstructor {
+  new (...arg: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
